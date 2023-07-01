ea02242dcbb277c84cd7d745647a794ba7dc0632
Moon613 <68190337+Moon613@users.noreply.github.com>
2023-06-30 23:58:54 -0400

    Smaller fin sprites for baby aquapedes

--- src/plugin/BabyAquapede/BabyAquapedeHooks.cs
+++ src/plugin/BabyAquapede/BabyAquapedeHooks.cs
@@ -1,4 +1,5 @@
 using MonoMod.RuntimeDetour;
+using RWCustom;
 using System;
 using UnityEngine;
 using static System.Reflection.BindingFlags;
@@ -16,7 +17,7 @@ namespace Chimeric
             new Hook(typeof(Centipede).GetMethod("get_AquaCenti", Public | NonPublic | Instance), (Func<Centipede, bool> orig, Centipede self) => self.Template.type == CreatureTemplateType.BabyAquapede || orig(self));
 
             On.Centipede.GenerateSize += GenerateSize;
-            On.CentipedeGraphics.InitiateSprites += InitiateSprites;
+            On.CentipedeGraphics.DrawSprites += DrawSprites;
         }
         public static float GenerateSize(On.Centipede.orig_GenerateSize orig, AbstractCreature abstrCrit) {
             if (abstrCrit.creatureTemplate.type == CreatureTemplateType.BabyAquapede) {
@@ -24,14 +25,40 @@ namespace Chimeric
             }
             return orig(abstrCrit);
         }
-        public static void InitiateSprites(On.CentipedeGraphics.orig_InitiateSprites orig, CentipedeGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam) {
-            orig(self, sLeaser, rCam);
-            for (int i = 0; i < 2; i++) {
-                for (int k = 0; k < self.wingPairs; k++) {
-                    Debug.Log($"i: {i} k: {k}");
-                    //(sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).MoveVertice(0, new Vector2((sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).vertices[0].x - 120f, (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).vertices[0].y));
-                    //sLeaser.sprites[self.WingSprite(i, k)].y += 50f;
-                    //sLeaser.sprites[self.WingSprite(i, k)].SetPosition(sLeaser.sprites[self.WingSprite(i, k)].GetPosition()+new Vector2(0, 50f));
+        public static void DrawSprites(On.CentipedeGraphics.orig_DrawSprites orig, CentipedeGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos) {
+            orig(self, sLeaser, rCam, timeStacker, camPos);
+            if (self.centipede.Template.type == CreatureTemplateType.BabyAquapede) {
+                for (int i = 0; i < 2; i++) {
+                    for (int k = 0; k < self.wingPairs; k++) {
+                        Vector2 vector10;
+                        if (k == 0)
+                        {
+                            vector10 = Custom.DirVec(self.ChunkDrawPos(0, timeStacker), self.ChunkDrawPos(1, timeStacker));
+                        }
+                        else
+                        {
+                            vector10 = Custom.DirVec(self.ChunkDrawPos(k - 1, timeStacker), self.ChunkDrawPos(k, timeStacker));
+                        }
+                        Vector2 vector11 = Custom.PerpendicularVector(vector10) / 2f;
+                        Vector2 vector12 = self.RotatAtChunk(k, timeStacker);
+                        Vector2 vector13 = self.WingPos(i, k, vector10, vector11, vector12, timeStacker);
+                        Vector2 vector14 = self.ChunkDrawPos(k, timeStacker) + self.centipede.bodyChunks[k].rad * ((i == 0) ? -1f : 1f) * vector11 * vector12.y;
+                        
+                        Vector2 lhs = Custom.DegToVec(Custom.AimFromOneVectorToAnother(vector13, vector14) + Custom.VecToDeg(vector12));
+                        float num12 = Mathf.InverseLerp(0.85f, 1f, Vector2.Dot(lhs, Custom.DegToVec(45f))) * Mathf.Abs(Vector2.Dot(Custom.DegToVec(45f + Custom.VecToDeg(vector12)), vector10));
+                        Vector2 lhs2 = Custom.DegToVec(Custom.AimFromOneVectorToAnother(vector14, vector13) + Custom.VecToDeg(vector12));
+                        float b2 = Mathf.InverseLerp(0.85f, 1f, Vector2.Dot(lhs2, Custom.DegToVec(45f))) * Mathf.Abs(Vector2.Dot(Custom.DegToVec(45f + Custom.VecToDeg(vector12)), -vector10));
+                        num12 = Mathf.Pow(Mathf.Max(num12, b2), 0.5f);
+                        float d7 = 1.75f;
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).MoveVertice(1, vector13 + vector10 * d7 - camPos);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).MoveVertice(0, vector13 - vector10 * d7 - camPos);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).MoveVertice(2, vector14 + vector10 * d7 - camPos);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).MoveVertice(3, vector14 - vector10 * d7 - camPos);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).verticeColors[0] = Custom.HSL2RGB(0.99f - 0.4f * Mathf.Pow(num12, 2f), 1f, 0.5f + 0.5f * num12, 0.5f + 0.5f * num12);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).verticeColors[1] = Custom.HSL2RGB(0.99f - 0.4f * Mathf.Pow(num12, 2f), 1f, 0.5f + 0.5f * num12, 0.5f + 0.5f * num12);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).verticeColors[2] = Color.Lerp(new Color(self.blackColor.r, self.blackColor.g, self.blackColor.b), new Color(1f, 1f, 1f), 0.5f * num12);
+                        (sLeaser.sprites[self.WingSprite(i, k)] as CustomFSprite).verticeColors[3] = Color.Lerp(new Color(self.blackColor.r, self.blackColor.g, self.blackColor.b), new Color(1f, 1f, 1f), 0.5f * num12);
+                    }
                 }
             }
         }
