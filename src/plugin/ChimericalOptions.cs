a614335835edcc2483a50c471932465bc9875a70
Moon613 <68190337+Moon613@users.noreply.github.com>
2023-08-01 15:50:04 -0400

    Add New Option for grabbing to Remix Config.

--- src/plugin/ChimericalOptions.cs
+++ src/plugin/ChimericalOptions.cs
@@ -8,15 +8,16 @@ namespace Chimeric
 {
     public class ChimericOptions : OptionInterface {
         private readonly ManualLogSource Logger;
-        [AllowNull] public static Configurable<KeyCode> tentMovementUp {get; set;}
-        [AllowNull] public static Configurable<KeyCode> tentMovementDown {get; set;}
-        [AllowNull] public static Configurable<KeyCode> tentMovementLeft {get; set;}
-        [AllowNull] public static Configurable<KeyCode> tentMovementRight {get; set;}
-        [AllowNull] public static Configurable<KeyCode> tentMovementEnable {get; set;}
-        [AllowNull] public static Configurable<KeyCode> tentMovementAutoEnable {get; set;}
-        [AllowNull] public static Configurable<bool> enableVignette {get; set;}
-        [AllowNull] public static Configurable<int> scareDuration {get; set;}
-        [AllowNull] public static Configurable<int> yeetusMagnitude {get; set;}
+        [AllowNull] public static Configurable<KeyCode> tentMovementUp { get; set; }
+        [AllowNull] public static Configurable<KeyCode> tentMovementDown { get; set; }
+        [AllowNull] public static Configurable<KeyCode> tentMovementLeft { get; set; }
+        [AllowNull] public static Configurable<KeyCode> tentMovementRight { get; set; }
+        [AllowNull] public static Configurable<KeyCode> tentMovementEnable { get; set; }
+        [AllowNull] public static Configurable<KeyCode> tentMovementAutoEnable { get; set; }
+        [AllowNull] public static Configurable<KeyCode> grabButton { get; set;}
+        [AllowNull] public static Configurable<bool> enableVignette { get; set; }
+        [AllowNull] public static Configurable<int> scareDuration { get; set; }
+        [AllowNull] public static Configurable<int> yeetusMagnitude { get; set; }
         [AllowNull] private UIelement[] UIRotArrPlayerOptions;
         [AllowNull] private UIelement[] UIDynoArrPlayerOptions;
 
@@ -28,6 +29,7 @@ namespace Chimeric
             tentMovementRight = config.Bind("tentacleMovementRight", new KeyCode());
             tentMovementEnable = config.Bind("tentacleMovementEnable", new KeyCode());
             tentMovementAutoEnable = config.Bind("tentacleMovementAutoEnable", new KeyCode());
+            grabButton = config.Bind("grabButton", new KeyCode());
             enableVignette = config.Bind("enableVignette", true);
             scareDuration = config.Bind("scareDuration", 120, new ConfigAcceptableRange<int>(40, 120));
             yeetusMagnitude = config.Bind("yeetusMagnitude", 22, new ConfigAcceptableRange<int>(5, 100));
@@ -48,29 +50,33 @@ namespace Chimeric
 
                 new OpLabel(200f, 520f, Translate("Movement Keybinds"), true),
                 new OpLabel(50f, 485f, Translate("Movement Tentacles Up"), false),
-                new OpKeyBinder(tentMovementUp, new Vector2(350f, 480f), new Vector2(120f, 30f)) {description = Translate("Tentacle Up")},
+                new OpKeyBinder(tentMovementUp, new Vector2(350f, 480f), new Vector2(120f, 30f)) {description = Translate("Tentacle Up.")},
                 new OpLabel(50f, 450f, Translate("Movement Tentacles Down"), false),
-                new OpKeyBinder(tentMovementDown, new Vector2(350f, 445f), new Vector2(120f, 30f)) {description = Translate("Tentacle Down")},
+                new OpKeyBinder(tentMovementDown, new Vector2(350f, 445f), new Vector2(120f, 30f)) {description = Translate("Tentacle Down.")},
                 new OpLabel(50f, 415f, Translate("Movement Tentacles Left"), false),
-                new OpKeyBinder(tentMovementLeft, new Vector2(350f, 410f), new Vector2(120f, 30f)) {description = Translate("Tentacle Left")},
+                new OpKeyBinder(tentMovementLeft, new Vector2(350f, 410f), new Vector2(120f, 30f)) {description = Translate("Tentacle Left.")},
                 new OpLabel(50f, 380f, Translate("Movement Tentacles Right"), false),
-                new OpKeyBinder(tentMovementRight, new Vector2(350f, 375f), new Vector2(120f, 30f)) {description = Translate("Tentacle Right")},
+                new OpKeyBinder(tentMovementRight, new Vector2(350f, 375f), new Vector2(120f, 30f)) {description = Translate("Tentacle Right.")},
                 new OpLabel(50f, 345f, Translate("Activate the tentacle movement"), false),
-                new OpKeyBinder(tentMovementEnable, new Vector2(350f, 340f), new Vector2(120f, 30f)) {description = Translate("Enable movement via tentacles")},
+                new OpKeyBinder(tentMovementEnable, new Vector2(350f, 340f), new Vector2(120f, 30f)) {description = Translate("Enable movement via tentacles.")},
                 new OpLabel(50f, 310f, Translate("Activate automatic tentacle movement"), false),
-                new OpKeyBinder(tentMovementAutoEnable, new Vector2(350f, 305f), new Vector2(120f, 30f)) {description = Translate("Activate tentacles without choosing an initial surface")},
-                new OpLabel(200f, 225f, Translate("Graphical Options"), true),
-                new OpLabel(50f, 175f, Translate("Enable Vignette effect"), false),
-                new OpCheckBox(enableVignette, new Vector2(400f, 170f))
+                new OpKeyBinder(tentMovementAutoEnable, new Vector2(350f, 305f), new Vector2(120f, 30f)) {description = Translate("Activate tentacles without choosing an initial surface.")},
+                new OpLabel(50f, 275f, Translate("Grab creatures"), false),
+                new OpKeyBinder(grabButton, new Vector2(350f, 270f), new Vector2(120f, 30f)) {description = Translate("Grab creatures with your tentacle when they are near enough.")},
+
+
+                new OpLabel(200f, 190f, Translate("Graphical Options"), true),
+                new OpLabel(50f, 150f, Translate("Enable Vignette effect"), false),
+                new OpCheckBox(enableVignette, new Vector2(400f, 145f)) {description = Translate("Play blinded, unable to see far from yourself.")}
             };
             UIelement[]? UIDynoArrPlayerOptions = new UIelement[]
             {
                 new OpLabel(260f, 570f, Translate("Options"), true),
 
                 new OpLabel(150f, 520f, Translate("Mimicry effectiveness"), false),
-                new OpSlider(scareDuration, new Vector2(330f, 515f), 80, false) {description = Translate("How effective the shock mimicry is")},
+                new OpSlider(scareDuration, new Vector2(330f, 515f), 80, false) {description = Translate("How effective the shock mimicry is.")},
                 new OpLabel(150f, 485f, Translate("Force of Tail"), false),
-                new OpSlider(yeetusMagnitude, new Vector2(330f, 480f), 80, false) {description = Translate("How much yeeting the tail does")}
+                new OpSlider(yeetusMagnitude, new Vector2(330f, 480f), 80, false) {description = Translate("How much yeeting the tail does.")}
             };
             rotTab.AddItems(UIRotArrPlayerOptions);
             dynoTab.AddItems(UIDynoArrPlayerOptions);
